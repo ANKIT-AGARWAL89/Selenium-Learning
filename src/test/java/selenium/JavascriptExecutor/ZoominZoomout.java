ede270978ba3c1f8452e5d1c8cf57f7d0587b718
Ankit Agarwal <ankit.agarwal@sky.uk>
2025-09-14 14:51:32 +0100

    Adding the code to zoom in and zoom out via JSEXECUTOR

---
+++ src/test/java/selenium/JavascriptExecutor/ZoominZoomout.java
@@ -0,0 +1,20 @@
+package selenium.JavascriptExecutor;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ZoominZoomout {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.body.style.zoom='50%'");
+
+        driver.quit();
+    }
+}
